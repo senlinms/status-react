823ec2889a0fdfafbb2be91c3d977101db00c2ac
Lukasz Fryc <fryc.lukasz@gmail.com>
2018-02-27 17:37:06 +0100

    Refactor Github test report

---
+++ test/appium/support/github_test_report.py
@@ -0,0 +1,189 @@
+import json
+from hashlib import md5
+import hmac
+import os
+
+from tests import SingleTestData
+
+
+class GithubHtmlReport:
+
+    TEST_REPORT_DIR = "%s/../report" % os.path.dirname(os.path.abspath(__file__))
+
+    def __init__(self, sauce_username, sauce_access_key):
+        self.sauce_username = sauce_username
+        self.sauce_access_key = sauce_access_key
+        self.init_report()
+
+    def init_report(self):
+        if not os.path.exists(self.TEST_REPORT_DIR):
+            os.makedirs(self.TEST_REPORT_DIR)
+        # delete all old files in report dir
+        file_list = [f for f in os.listdir(self.TEST_REPORT_DIR)]
+        for f in file_list:
+            os.remove(os.path.join(self.TEST_REPORT_DIR, f))
+
+    def get_test_report_file_path(self, test_name):
+        file_name = "%s.json" % test_name
+        return os.path.join(self.TEST_REPORT_DIR, file_name)
+
+    def build_html_report(self):
+        tests = self.get_all_tests()
+        passed_tests = self.get_passed_tests()
+        failed_tests = self.get_failed_tests()
+
+        if len(tests) > 0:
+            title_html = "<h2>%.0f%% of end-end tests have passed</h2>" % (len(passed_tests) / len(tests) * 100)
+            summary_html = "<code>"
+            summary_html += "Total executed tests: %d<br />" % len(tests)
+            summary_html += "Failed tests: %d" % len(failed_tests)
+            summary_html += "</code>"
+            failed_tests_html = str()
+            passed_tests_html = str()
+            if failed_tests:
+                failed_tests_html = self.build_failed_tests_html_table(failed_tests)
+            if passed_tests:
+                passed_tests_html = self.build_passed_tests_html_table(passed_tests)
+            return title_html + summary_html + failed_tests_html + passed_tests_html
+        else:
+            return None
+
+    def save_test(self, test):
+        file_path = self.get_test_report_file_path(test.name)
+        json.dump(test.__dict__, open(file_path, 'w'))
+
+    def get_all_tests(self):
+        tests = list()
+        file_list = [f for f in os.listdir(self.TEST_REPORT_DIR)]
+        for file_name in file_list:
+            file_path = os.path.join(self.TEST_REPORT_DIR, file_name)
+            test_dict = json.load(open(file_path))
+            tests.append(SingleTestData(name=test_dict['name'], steps=test_dict['steps'],
+                                        jobs=test_dict['jobs'], error=test_dict['error']))
+        return tests
+
+    def get_failed_tests(self):
+        tests = self.get_all_tests()
+        failed = list()
+        for test in tests:
+            if test.error is not None:
+                failed.append(test)
+        return failed
+
+    def get_passed_tests(self):
+        tests = self.get_all_tests()
+        passed = list()
+        for test in tests:
+            if test.error is None:
+                passed.append(test)
+        return passed
+
+    def build_passed_tests_html_table(self, passed_tests):
+        tests = "<h3>Passed tests (%d)</h3>" % len(passed_tests)
+        tests += "<details>"
+        tests += "<summary>Click to expand</summary>"
+        tests += "<br/>"
+        tests += "<table style=\"width: 100%\">"
+        tests += "<colgroup>"
+        tests += "<col span=\"1\" style=\"width: 20%;\">"
+        tests += "<col span=\"1\" style=\"width: 80%;\">"
+        tests += "</colgroup>"
+        tests += "<tbody>"
+        tests += "<tr>"
+        tests += "<th>Test</th>"
+        tests += "<th>Logs</th>"
+        tests += "</tr>"
+        for test in passed_tests:
+            tests += self.build_passed_test_html_table_row(test)
+        tests += "</tbody>"
+        tests += "</table>"
+        tests += "</details>"
+        return tests
+
+    def build_passed_test_html_table_row(self, passed_test):
+        test_steps_html = str()
+        for step in passed_test.steps:
+            test_steps_html += "<div>%s</div>" % step
+
+        row = "<tr>"
+        row += "<td>%s</td>" % passed_test.name
+        row += "<td>"
+        row += "<p>"
+        row += "<details><summary>Click to expand full logs</summary>"
+        row += "<br/>"
+        if test_steps_html:
+            row += "<p><ins>Test steps:</ins></p>"
+            row += "<blockquote>"
+            row += "%s" % test_steps_html
+            row += "</blockquote>"
+        if passed_test.jobs:
+            row += "<hr/>"
+            row += self.build_device_sessions_html(passed_test.jobs)
+
+        row += "</details>"
+        row += "</p>"
+        row += "</td></tr>"
+        return row
+
+    def build_failed_tests_html_table(self, failed_tests):
+        tests = "<h3>Failed tests (%d)</h3>" % len(failed_tests)
+        tests += "<table style=\"width: 100%\">"
+        tests += "<colgroup>"
+        tests += "<col span=\"1\" style=\"width: 20%;\">"
+        tests += "<col span=\"1\" style=\"width: 80%;\">"
+        tests += "</colgroup>"
+        tests += "<tbody>"
+        tests += "<tr>"
+        tests += "<th>Test</th>"
+        tests += "<th>Failure</th>"
+        tests += "</tr>"
+        for test in failed_tests:
+            tests += self.build_failed_test_html_table_row(test)
+        tests += "</tbody>"
+        tests += "</table>"
+        return tests
+
+    def build_failed_test_html_table_row(self, failed_test):
+        test_steps_html = list()
+        for step in failed_test.steps:
+            test_steps_html.append("<div>%s</div>" % step)
+        row = "<tr>"
+        row += "<td>%s</td>" % failed_test.name
+        row += "<td>"
+        if test_steps_html:
+            row += "<p>"
+            row += "<blockquote>"
+            # last 3 steps as summary
+            row += "%s" % ''.join(test_steps_html[-3:])
+            row += "</blockquote>"
+            row += "</p>"
+            row += "<p>"
+            row += "<details><summary>Click to expand full logs</summary>"
+            row += "<br/>"
+            row += "<p><ins>Test steps:</ins></p>"
+            row += "<blockquote>"
+            row += "%s" % ''.join(test_steps_html)
+            row += "</blockquote>"
+        if failed_test.error:
+            row += "<code>%s</code>" % failed_test.error
+        if failed_test.jobs:
+            row += "<hr/>"
+            row += self.build_device_sessions_html(failed_test.jobs)
+        if test_steps_html:
+            row += "</details>"
+            row += "</p>"
+        row += "</td></tr>"
+        return row
+
+    def get_sauce_job_url(self, job_id):
+        token = hmac.new(bytes(self.sauce_username + ":" + self.sauce_access_key, 'latin-1'),
+                         bytes(job_id, 'latin-1'), md5).hexdigest()
+        return "https://saucelabs.com/jobs/%s?auth=%s" % (job_id, token)
+
+    def build_device_sessions_html(self, jobs):
+        html = "<ins>Device sessions:</ins>"
+        html += "<p><ul>"
+        for job_id in jobs:
+            html += "<li><a href=\"%s\"></a></li>" % self.get_sauce_job_url(job_id)
+        html += "</p></ul>"
+        return html
--- test/appium/tests/conftest.py
+++ test/appium/tests/conftest.py
@@ -1,4 +1,4 @@
-from tests import test_data
+from tests import test_suite_data, SingleTestData
 import requests
 import re
 import pytest
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import environ
 from io import BytesIO
 from sauceclient import SauceClient
-from tests.github_test_report import GithubHtmlReport
+from support.github_test_report import GithubHtmlReport
 
 storage = 'http://artifacts.status.im:8081/artifactory/nightlies-local/'
 
@@ -15,6 +15,7 @@ sauce_access_key = environ.get('SAUCE_ACCESS_KEY')
 github_token = environ.get('GIT_HUB_TOKEN')
 
 sauce = SauceClient(sauce_username, sauce_access_key)
+github_report = GithubHtmlReport(sauce_username, sauce_access_key)
 
 
 def get_latest_apk():
@@ -59,7 +60,7 @@ def is_master(config):
 def is_uploaded():
     stored_files = sauce.storage.get_stored_files()
     for i in range(len(stored_files['files'])):
-        if stored_files['files'][i]['name'] == test_data.apk_name:
+        if stored_files['files'][i]['name'] == test_suite_data.apk_name:
             return True
 
 
@@ -67,8 +68,8 @@ def pytest_configure(config):
     if config.getoption('log'):
         import logging
         logging.basicConfig(level=logging.INFO)
-    test_data.apk_name = ([i for i in [i for i in config.getoption('apk').split('/')
-                                       if '.apk' in i]])[0]
+    test_suite_data.apk_name = ([i for i in [i for i in config.getoption('apk').split('/')
+                                             if '.apk' in i]])[0]
     if is_master(config) and config.getoption('env') == 'sauce':
         if config.getoption('pr_number'):
             with open('github_comment.txt', 'w') as _:
@@ -80,7 +81,7 @@ def pytest_configure(config):
                 file = BytesIO(response.content)
                 del response
                 requests.post('http://saucelabs.com/rest/v1/storage/'
-                              + sauce_username + '/' + test_data.apk_name + '?overwrite=true',
+                              + sauce_username + '/' + test_suite_data.apk_name + '?overwrite=true',
                               auth=(sauce_username, sauce_access_key),
                               data=file,
                               headers={'Content-Type': 'application/octet-stream'})
@@ -89,32 +90,11 @@ def pytest_configure(config):
 
 
 def pytest_unconfigure(config):
-    github_report = GithubHtmlReport(test_data, sauce_username, sauce_access_key)
-    print(github_report.build_html_report())
     if is_master(config) and config.getoption('pr_number'):
         from github import Github
         repo = Github(github_token).get_user('status-im').get_repo('status-react')
         pull = repo.get_pull(int(config.getoption('pr_number')))
         pull.create_issue_comment(github_report.build_html_report())
-        # with open('github_comment.txt', 'r') as comment:
-        #     pull.create_issue_comment('# Automated test results: \n' + comment.read())
-
-
-
-def make_github_report(error=None):
-    pass
-    # if pytest.config.getoption('pr_number'):
-    #     title = '### %s' % test_data.test_name
-    #     outcome = '%s' % ':x:' if error else ':white_check_mark:' + ':\n'
-    #     title += outcome
-    #     steps = '\n\n <details>\n<summary>Test Steps & Error message:</summary>\n\n ```%s ```%s\n\n</details>\n' % \
-    #             (test_data.test_info[test_data.test_name]['steps'], '\n```' + error + '```' if error else '')
-    #     sessions = str()
-    #
-    #     for job_id in test_data.test_info[test_data.test_name]['jobs']:
-    #         sessions += '  - [Android Device Session](%s) \n' % get_public_url(job_id)
-    #     with open('github_comment.txt', 'a') as comment:
-    #         comment.write(title + '\n' + steps + '\n' + sessions + '---\n')
 
 
 @pytest.mark.hookwrapper
@@ -122,17 +102,19 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
     is_sauce_env = pytest.config.getoption('env') == 'sauce'
+    current_test = test_suite_data.current_test
     if report.when == 'call':
         if report.failed:
-            test_data.test_info[test_data.test_name]['error'] = report.longreprtext
+            current_test.error = report.longreprtext
         if is_sauce_env:
-            update_sauce_jobs(test_data, report.passed)
+            update_sauce_jobs(current_test.name, current_test.jobs, report.passed)
+        github_report.save_test(current_test)
 
 
-def update_sauce_jobs(test_data, passed):
-    for job_id in test_data.test_info[test_data.test_name]['jobs']:
-        sauce.jobs.update_job(job_id, name=test_data.test_name, passed=passed)
+def update_sauce_jobs(test_name, job_ids, passed):
+    for job_id in job_ids:
+        sauce.jobs.update_job(job_id, name=test_name, passed=passed)
 
 
 def pytest_runtest_setup(item):
-    test_data.test_name = item.name
+    test_suite_data.add_test(SingleTestData(item.name))
